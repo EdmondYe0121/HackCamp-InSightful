030b01a8912cbadafc39af7a52d0f76293a83f34
kckoh <kyungcheolkoh@gmail.com>
2023-11-19 15:59:20 -0800

    change dyslexia font style to courier

--- scripts/fontStyleFunction/changeFontVerdana.js
+++ scripts/fontStyleFunction/changeFontVerdana.js
@@ -2,7 +2,7 @@ function changeFontVerdana() {
 
     var allElements = document.querySelectorAll('*');
     allElements.forEach(function(element) {
-        element.style.fontFamily = 'Verdana, sans-serif'; // Set font to Verdana, concatenate strings
+        element.style.fontFamily = 'Courier, sans-serif'; // Set font to Verdana, concatenate strings
         element.style.letterSpacing = '0px'; // Correcting letter spacing
     });
 
